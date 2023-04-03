4171c48bb658d2c2a495c46faa5e475ed08a2693
12598511 <77835584+dungnguyenhd@users.noreply.github.com>
2023-04-03 16:44:21 +0700

    git2

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,7 @@ module.exports = {
       ? path.join(__dirname, './public/index.js')
       : path.join(__dirname, './src/index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    path: path.join(__dirname, '/lib'),
+    path: path.join(__dirname, '/dist'),
     filename: 'index.js',
     libraryTarget: 'umd', // 采用通用模块定义
     libraryExport: 'default', // 兼容 ES6 的模块系统、CommonJS 和 AMD 模块规范
